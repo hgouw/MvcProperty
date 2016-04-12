f60b460fbfe770d30bad2b96804d90ec919d452c
Herman Gouw <hermangouw@gmail.com>
2016-04-13 09:17:37 +1000

    Initial Version

---
+++ Controllers/PropertyController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcApplication1.Models;
+
+namespace MvcApplication1.Controllers
+{
+    public class PropertyController : Controller
+    {
+        private PropertyDBContext db = new PropertyDBContext();
+
+        //
+        // GET: /Property/
+
+        public ActionResult Index()
+        {
+            return View(db.Properties.ToList());
+        }
+
+        //
+        // GET: /Property/Details/5
+
+        public ActionResult Details(string street = null, string number = null, string suburb = null, string state = null, string postcode = null)
+        {
+            Property property = db.Properties.Find(street, number, suburb, state, postcode);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+
+        //
+        // GET: /Property/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /Property/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Property property)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Properties.Add(property);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(property);
+        }
+
+        //
+        // GET: /Property/Edit/5
+
+        public ActionResult Edit(string street = null, string number = null, string suburb = null, string state = null, string postcode = null)
+        {
+            Property property = db.Properties.Find(street, number, suburb, state, postcode);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+
+        //
+        // POST: /Property/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(Property property)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(property).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(property);
+        }
+
+        //
+        // GET: /Property/Delete/5
+
+        public ActionResult Delete(string street = null, string number = null, string suburb = null, string state = null, string postcode = null)
+        {
+            Property property = db.Properties.Find(street, number, suburb, state, postcode);
+            if (property == null)
+            {
+                return HttpNotFound();
+            }
+            return View(property);
+        }
+
+        //
+        // POST: /Property/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string street = null, string number = null, string suburb = null, string state = null, string postcode = null)
+        {
+            Property property = db.Properties.Find(street, number, suburb, state, postcode);
+            db.Properties.Remove(property);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Models/PropertyModels.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MvcApplication1.Models
+{
+    public class PropertyDBContext : DbContext
+    {
+        public DbSet<Property> Properties { get; set; }
+    }
+
+    public class Property
+    {
+        [Key, Column(Order = 1)]
+        public string Street { get; set; }
+        [Key, Column(Order = 2)]
+        public string Number { get; set; }
+        [Key, Column(Order = 3)]
+        public string Suburb { get; set; }
+        [Key, Column(Order = 4)]
+        public string State { get; set; }
+        [Key, Column(Order = 5)]
+        public string Postcode { get; set; }
+        public byte Type { get; set; }
+        public decimal Price { get; set; }
+        public DateTime SoldDate { get; set; }
+    }
+}
